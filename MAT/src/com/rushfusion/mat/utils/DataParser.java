9893f95171f2529f82eee9c548633e1d9d05f46a
lsm <78783606@qq.com>
2012-06-25 14:20:18 +0800

    ..

--- MAT/src/com/rushfusion/mat/utils/DataParser.java
+++ MAT/src/com/rushfusion/mat/utils/DataParser.java
@@ -1,5 +1,44 @@
 package com.rushfusion.mat.utils;
 
+import java.util.List;
+
+import android.content.Context;
+
 public class DataParser {
+	
+	private static DataParser parser;
+	private static Context mContext;
+	private DataParser() {
+		
+	}
+	
+	public static DataParser getInstance(Context context){
+		if(parser==null){
+			mContext = context;
+			parser = new DataParser();
+		}
+		return parser;
+	}
+	
+	public List<String> getCategory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getTypes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Integer> getYears() {
+		// TODO Auto-generated method stub
+		return null;
+		
+	}
+
+	public List<String> getAreas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
