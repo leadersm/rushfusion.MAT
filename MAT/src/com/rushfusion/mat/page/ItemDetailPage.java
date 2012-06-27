d0f385cf83417b2a7d9b79cb764c936769304339
liyang <gustav@163.com>
2012-06-27 11:11:02 +0800

    vvv

--- MAT/src/com/rushfusion/mat/page/ItemDetailPage.java
+++ MAT/src/com/rushfusion/mat/page/ItemDetailPage.java
@@ -39,7 +39,9 @@ public class ItemDetailPage extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.page_item_detail);
 		Movie movie = (Movie) getIntent().getSerializableExtra("movieInfo");
-		
+		if(movie==null){
+			movie = new Movie(1, 21, 85, 3422, "teleplay", "栗洋的生活", "现代", 2012, "gustav", "wangying", "大陆", "sldhglnoengdinrkgndkrgnkdngrkdnskalnrnekl", "http://cache.mars.sina.com.cn/nd/movievideo/thumb/39/3739_mc.jpg", "2:03:02", "1|http://v.iask.com/v_play_ipad.php?vid=71014567^2|http://v.iask.com/v_play_ipad.php?vid=71014693^3|http://v.iask.com/v_play_ipad.php?vid=71063490^4|http://v.iask.com/v_play_ipad.php?vid=71064184^5|http://v.iask.com/v_play_ipad.php?vid=71115062^6|http://v.iask.com/v_play_ipad.php?vid=71114760^7|http://v.iask.com/v_play_ipad.php?vid=71169821^8|http://v.iask.com/v_play_ipad.php?vid=71163134^9|http://v.iask.com/v_play_ipad.php?vid=71233199^10|http://v.iask.com/v_play_ipad.php?vid=71232205^11|http://v.iask.com/v_play_ipad.php?vid=71284679^12|http://v.iask.com/v_play_ipad.php?vid=71284659^13|http://v.iask.com/v_play_ipad.php?vid=71340181^14|http://v.iask.com/v_play_ipad.php?vid=71341611^15|http://v.iask.com/v_play_ipad.php?vid=71395392^16|http://v.iask.com/v_play_ipad.php?vid=71395892^17|http://v.iask.com/v_play_ipad.php?vid=71451799^18|http://v.iask.com/v_play_ipad.php?vid=71453704^19|http://v.iask.com/v_play_ipad.php?vid=71496952^20|http://v.iask.com/v_play_ipad.php?vid=71497152^21|http://v.iask.com/v_play_ipad.php?vid=71538687^22|http://v.iask.com/v_play_ipad.php?vid=71539613^23|http://v.iask.com/v_play_ipad.php?vid=71604823^24|http://v.iask.com/v_play_ipad.php?vid=71606834^25|http://v.iask.com/v_play_ipad.php?vid=71655172^26|http://v.iask.com/v_play_ipad.php?vid=71655382^27|http://v.iask.com/v_play_ipad.php?vid=71698978^28|http://v.iask.com/v_play_ipad.php?vid=71699284^29|http://v.iask.com/v_play_ipad.php?vid=71757029^30|http://v.iask.com/v_play_ipad.php?vid=71756847^31|http://v.iask.com/v_play_ipad.php?vid=71777143^32|http://v.iask.com/v_play_ipad.php?vid=71777214^35|http://v.iask.com/v_play_ipad.php?vid=71817577^36|http://v.iask.com/v_play_ipad.php?vid=71817747^37|http://v.iask.com/v_play_ipad.php?vid=71817352", 12, 1555, 12);
+		}
 		image = (ImageView) findViewById(R.id.page_item_detail_image);
 		name = (TextView) findViewById(R.id.page_item_detail_name);
 		description = (TextView) findViewById(R.id.page_item_detail_description);
@@ -49,8 +51,6 @@ public class ItemDetailPage extends Activity{
 		directors = (TextView) findViewById(R.id.page_item_detail_directors);
 		artists = (TextView) findViewById(R.id.page_item_detail_artists);
 		episode = (GridView) findViewById(R.id.page_item_detail_episode);
-		
-		
 		image.setImageURI(Uri.parse(movie.getThumb()));
 		image.setScaleType(ScaleType.FIT_XY);
 		name.setText(movie.getName());
@@ -63,11 +63,12 @@ public class ItemDetailPage extends Activity{
 		if(movie.getUrl().indexOf("^")==-1){
 			list.add(movie.getUrl());
 		}else{
-			String[] str = movie.getUrl().split("^");
+			String[] str = movie.getUrl().split("\\^");
 			list = new ArrayList<String>();
 			for(String urlstr : str){
+				System.out.println("===>"+urlstr);
 				System.out.println(urlstr);
-				String[] str2 = urlstr.split("|");
+				String[] str2 = urlstr.split("\\|");
 				System.out.println(str2[1]);
 				list.add(str2[1]);
 			}
