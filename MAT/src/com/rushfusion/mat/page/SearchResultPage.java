ddfb6de8743948c04475774a937ee1a9858a3d7b
lsm <78783606@qq.com>
2012-07-03 09:01:47 +0800

    merge liyang and add the search images into index

---
+++ MAT/src/com/rushfusion/mat/page/SearchResultPage.java
@@ -0,0 +1,14 @@
+package com.rushfusion.mat.page;
+
+import android.app.Activity;
+import android.view.ViewGroup;
+
+public class SearchResultPage extends FilmClassPage {
+
+	public SearchResultPage(Activity context, ViewGroup parent) {
+		super(context, parent);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
