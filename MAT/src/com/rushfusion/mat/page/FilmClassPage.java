893f109b8a441cc9c6790f04d7da17386645e683
chengchuanshuai <chbjsd@hotmail.com>
2012-07-04 12:03:55 +0800

    ....test

--- MAT/src/com/rushfusion/mat/page/FilmClassPage.java
+++ MAT/src/com/rushfusion/mat/page/FilmClassPage.java
@@ -20,6 +20,7 @@ import com.rushfusion.mat.utils.ImageLoadTask;
 import com.rushfusion.mat.video.entity.Movie;
 
 public class FilmClassPage extends BasePage {
+	//phone test
 	private static final int FILM_NUM = 8 ;
 	private Context mContext ;
 	private ViewGroup mParent ;
